d59c6dfbd6a60f0f03c1159d58bb6ee97d604a9e
btssio <btssio@btssio-VirtualBox>
2016-10-19 16:03:07 +0200

    Création Classe Attribution et Test Offre et Attribution

---
+++ test/TestAttribution.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Attribution Test</title>
+    </head>
+    <body>
+        <?php
+        use modele\metier\Attribution;
+        require_once __DIR__ . '/../includes/autoload.php';
+        echo "<h2>Test unitaire de la classe métier Attribution/h2>";
+        $objet = new Offre('9999999A', 'C2', '5fsq',4);
+        var_dump($objet);
+        ?>
+    </body>
+</html>
+
